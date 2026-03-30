214c8e1f8ab525edbe00136eba1c512be2b597f8
PrathamChiragGhosh <core.pratham@gmail.com>
2026-03-30 14:03:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ import applicationRoutes from "./routes/applicationRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://jobs.careersatcore.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/jobs", jobRoutes);
