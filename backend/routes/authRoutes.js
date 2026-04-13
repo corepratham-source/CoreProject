204c9a93f14b10bc714a32bea2e9a2503c9ac2e4
PrathamChiragGhosh <core.pratham@gmail.com>
2026-04-13 17:22:20 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup, logout, me } from "../controllers/authController.js";
+import { login, signup, logout, adminLogin, me } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.get("/me", protect, me);
+router.post("/admin-login", adminLogin);
 
 export default router;
